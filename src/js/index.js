0907affbd91e0faa734a866320caaac6b8ee60ce
Villeman <villeman.arm@gmail.com>
2023-10-04 13:27:32 +0300

    events block complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,7 @@
 'use strict';
 
-// import openBlock from "./modules/open_block";
+import openBlock from "./modules/slider";
 
-// import Swiper from './libraries/swiper-bundle';
-// import { Navigation, Pagination } from './libraries/swiper-bundle';
 
-const swiper = new Swiper('.slider', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
 
---
+++ src/js/modules/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    loop: true,
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
